55239b9c9ae7b177327d9d4ee9cd93b6c84ad557
G.Hemanth Raaj <hgirirajan@teamhgs.com>
2024-01-14 16:31:06 +0530

    Added Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./style.css";
 
 export const Header = () => {
+  const headContentList = [
+    { index: "01.", title: "About" },
+    { index: "02.", title: "Experience" },
+    { index: "03.", title: "Work" },
+    { index: "04.", title: "Contact" },
+  ];
   return (
-    <div>Header</div>
-  )
-}
+    <div id="header_component">
+      <div id="header">
+        <p id="header_logo">H</p>
+        <div id="right_header">
+          <div id="right_header_item">
+            {headContentList.map((item) => (
+              <a href="#">
+                <p className="head_item" key={item.index}>
+                  <span className="head_num">{item.index}</span>
+                  <span className="head_title">{item.title}</span>
+                </p>
+              </a>
+            ))}
+          </div>
+          <p id="head_resume">
+            <a href="#">Resume</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -3,12 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage/index";
 import { NotFoundPage } from "../pages/NotFoundPage/index";
 import { ArchievePage } from "../pages/ArchievePage/index";
+import { Header } from "../components/Header";
 
 export const Home = () => {
   return (
     <div>
       <BrowserRouter>
-        <section>Header</section>
+        <Header />
         <section>Right nav</section>
         <section>Left Nav</section>
         <main>
