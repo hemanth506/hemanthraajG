3e934f3d89257e319a6c080931f3dbeac39fc709
G.Hemanth Raaj <hgirirajan@teamhgs.com>
2024-01-14 20:54:49 +0530

    About setup

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { Intro } from '../../components/Intro'
-import "./style.css"
+import React from "react";
+import { Intro } from "../../components/Intro";
+import "./style.css";
+import { About } from "../../components/About";
 
 export const HomePage = () => {
   return (
-    <div id='home_page_main_content'>
-      <div id='contents'>
+    <div id="home_page_main_content">
+      <div id="contents">
         <Intro />
+        <About />
       </div>
     </div>
-  )
-}
+  );
+};
